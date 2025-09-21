0fa4363de18570773073894765010a06d37ef3ee
Bombor <ezazemailmegnemfoglalt@gmail.com>
2025-09-21 14:00:24 +0200

    Add files via upload

---
+++ components/end_menu_bad.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var retry_btn = $Panel/Retry/Button
+@onready var exit_btn   = $Panel/Exit/Button2
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS   
+	visible = false
+
+	retry_btn.pressed.connect(_on_retry_pressed)
+	exit_btn.pressed.connect(_on_exit_pressed)
+
+func _on_retry_pressed():
+	get_tree().reload_current_scene()
+
+func _on_exit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://menu/MainMenu.tscn")
